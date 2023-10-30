4f778fae5ed5377ee17f2039bb07707623d71315
Petr Vojta <petvoj@gmail.com>
2023-10-30 22:35:49 +0100

    Restore original website tracedo.com

---
+++ assets/javascripts/scripts.js
@@ -0,0 +1,6 @@
+//= require ./jquery-1.11.0.js
+//= require ./bootstrap.min.js
+//= require ./jquery.easing.min.js
+//= require ./classie.js
+//= require ./cbpAnimatedHeader.js
+//= require ./agency.js
